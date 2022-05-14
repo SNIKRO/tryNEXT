db2591c2964226247886ac6c05f18e66e4d70b95
SNIKR <snikr@bk.ru>
2022-05-14 16:21:34 +0600

    add api request, take data and show on html

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,13 +1,28 @@
 import { useRouter } from "next/router"
+import Link from 'next/link'
 import { MainLayout } from "../../components/MainLayout"
 
-export default function Post() {
+export default function Post({ post }) {
 
-  const router = useRouter()
+
+  //const router = useRouter()
+
 
   return(
     <MainLayout>
-      <h1>Post {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <div>
+        {post.body}
+      </div>
+      <Link href={'/posts'}><a>See all posts</a></Link>
     </MainLayout>
   )
-};
+}
+
+Post.getInitialProps = async (ctx) => {
+  const response = await fetch(`http://localhost:4200/post/${ctx.query.id}`)
+  const post = await response.json()
+  return {
+    post
+  }
+}
--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,45 @@
-import { MainLayout } from "../components/MainLayout";
+import {useState, useEffect} from 'react'
+import { MainLayout } from "../components/MainLayout"
+import Link from "next/link"
 
-export default function Posts() {
+export default function Posts({ posts }) {
+
+  //const [posts, setPosts] = useState([])
+
+  // useEffect(() => {
+  //   async function load() {
+  //     const response = await fetch('http://localhost:4200/posts')
+  //     const json = await response.json()
+  //     setPosts(json)
+  //   }
+  //   load()
+  // }, [])
+  
+
   return (
     <MainLayout>
       <h1>Posts Pages</h1>
+      <ul>
+        {posts.map( post => (
+          <li key = {post.id}>
+            <Link href = {`/post/[id]`} as = {`/post/${post.id}`}>
+              <a>
+                {post.title}
+              </a>            
+            </Link>
+          </li>
+        )
+        )
+        }
+      </ul>
     </MainLayout>
   )
-};
+}
+
+Posts.getInitialProps = async () => {
+      const response = await fetch('http://localhost:4200/posts')
+      const posts = await response.json()
+      return {
+        posts
+      }
+}
